17ac6668d17349d077a02d61901435401c554835
NatalieC2k <nataliec2k5@gmail.com>
2024-09-25 23:33:58 -0230

    it now do the thing?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,15 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM node:lts AS runtime
-WORKDIR /app
+WORKDIR /
 
 COPY . .
 
