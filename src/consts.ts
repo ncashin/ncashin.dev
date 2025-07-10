526ed27378406e813b670970d0a4f868a21b7849
Natalie Cashin <nataliec2k5@gmail.com>
2025-07-09 22:41:39 -0230

    innovative technology

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,5 @@
-export const SITE_TITLE = "ncashin.dev";
+export const SITE_TITLE =
+  process.env.NODE_ENV === "development"
+    ? "ncashin.dev - local"
+    : "ncashin.dev";
 export const SITE_DESCRIPTION = "My personal site";
