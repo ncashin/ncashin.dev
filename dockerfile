8fcd2515f1c0ae6002113ba13cc610f2891f654b
Natalie Cashin <nataliec2k5@gmail.com>
2025-07-09 22:52:47 -0230

    being lazy about ports

--- dockerfile
+++ dockerfile
@@ -18,6 +18,6 @@ COPY --from=prod-deps /app/node_modules ./node_modules
 COPY --from=build /app/dist ./dist
 
 ENV HOST=0.0.0.0
-ENV PORT=80
-EXPOSE 80
+ENV PORT=4321
+EXPOSE 4321
 CMD node ./dist/server/entry.mjs
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-export const SITE_TITLE =
-  process.env.NODE_ENV === "development"
-    ? "ncashin.dev - local"
-    : "ncashin.dev";
+export const SITE_TITLE = import.meta.env.DEV
+  ? "ncashin.dev - local"
+  : "ncashin.dev";
 export const SITE_DESCRIPTION = "My personal site";
