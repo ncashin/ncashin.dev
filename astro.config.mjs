217bc12f494ce09a639dcddf8623e2d8339c26c6
ncashin <nataliec2k5@gmail.com>
2026-04-16 16:11:18 -0230

    fix embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://ncashin.dev',
   vite: {
     plugins: [tailwindcss()]
   }
